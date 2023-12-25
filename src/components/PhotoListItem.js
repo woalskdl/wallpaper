4f6c624a2ea4678ba49292f1b7331e74a6cafcfd
jay <woalskdl@gmail.com>
2023-12-26 00:04:20 +0900

    디테일 - 다운로드, 사진 클릭 애니메이션 추거

--- src/components/PhotoListItem.js
+++ src/components/PhotoListItem.js
@@ -1,5 +1,5 @@
-import React, { useCallback } from "react";
-import { useWindowDimensions } from "react-native";
+import React, { useCallback, useState } from "react";
+import { Animated, useWindowDimensions } from "react-native";
 import { Button } from './Button';
 import { RemoteImage } from "./RemoteImage";
 import { useNavigation } from "@react-navigation/native";
@@ -9,14 +9,48 @@ export const PhotoListItem = (props) => {
 
     const { width } = useWindowDimensions();
     const navigation = useNavigation();
+    const [animValue] = useState(new Animated.Value(0));
 
     const onPressItem = useCallback(() => {
         navigation.navigate('ImageDetail', {url:props.url})
     }, []);
 
+    const onPressIn = useCallback(() => {
+        Animated.timing(animValue, {
+            duration:200,
+            toValue:1,
+            useNativeDriver: true
+        }).start();
+    }, []);
+
+    const onPressOut = useCallback(() => {
+        Animated.timing(animValue, {
+            duration:200,
+            toValue:0,
+            useNativeDriver: true
+        }).start();
+    }, []);
+
+    const scale = animValue.interpolate({
+        inputRange:[0, 1],
+        outputRange: [1.0, 0.95]
+    })
+
     return (
-        <Button onPress={onPressItem} paddingHorizontal={20} paddingVertical={10}>
-            <RemoteImage url={props.url} width={width - 40} height={width * 1.2}/>
+        <Button 
+            onPress={onPressItem} 
+            onPressIn={onPressIn}
+            onPressOut={onPressOut}
+            paddingHorizontal={20} 
+            paddingVertical={10}
+        >
+            <Animated.View style={{transform:[{scale:scale}]}}>
+                <RemoteImage 
+                    url={props.url} 
+                    width={width - 40}
+                    height={width * 1.2}
+                />
+            </Animated.View>
         </Button>
     )
 }
--- src/screen/ImageDetailScreen.js
+++ src/screen/ImageDetailScreen.js
@@ -1,7 +1,7 @@
-import { View, useWindowDimensions } from 'react-native';
+import { ActivityIndicator, View, useWindowDimensions } from 'react-native';
 import { Typography } from '../components/Typography';
 import { Header } from '../components/header/Header';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { RemoteImage } from '../components/RemoteImage';
 import { Button } from '../components/Button';
@@ -12,12 +12,15 @@ import * as MediaLibrary from 'expo-media-library';
 export const ImageDetailScreen = (props) => {
   const navigation = useNavigation();
   const route = useRoute();
+  const [downloading, setDownloading] = useState(false);
 
   const onPressBack = useCallback(() => {
     navigation.goBack();
   }, []);
 
   const onPressDownload = useCallback(async () => {
+    setDownloading(true);
+
     const downloadResumable = FileSystem.createDownloadResumable(
       route.params.url,
       `${FileSystem.documentDirectory}${new Date().getMilliseconds()}.jpg`
@@ -25,32 +28,31 @@ export const ImageDetailScreen = (props) => {
 
     try {
       const { uri } = await downloadResumable.downloadAsync();
-      console.log('Finished download to ', uri);
 
       const permissionResponse = await MediaLibrary.getPermissionsAsync(true);
-      console.log('permissionResponse', permissionResponse);
 
       if(permissionResponse.status === 'denied') {
         // 아예 못쓰는 상태
+        setDownloading(false);
         return;
       } else if (permissionResponse.status = 'undetermined') {
         const requestResult = await MediaLibrary.requestPermissionsAsync();
-        console.log('requestResult >> ', requestResult);
 
         if (requestResult.status === 'denied') {
+          setDownloading(false);
           return;
         }
       }
 
       const asset = await MediaLibrary.createAssetAsync(uri);
-      const album = MediaLibrary.createAlbumAsync('MyFirstAlbum', asset, false);
+      const album = MediaLibrary.createAlbumAsync('MyFirstAlbum', asset, false);  // TODO : 다운로드 버튼 누를때마다 같은 이름 폴더 계속 생성중
 
-      console.log(album);
-      
     } catch(ex) {
 
     }
 
+    setDownloading(false);
+    
   }, []);
 
   const { width } = useWindowDimensions();
@@ -69,12 +71,18 @@ export const ImageDetailScreen = (props) => {
 
       <Button onPress={onPressDownload}>
         <View style={{paddingBottom:24, backgroundColor:'black'}}>
-          <View style={{ height:52, flexDirection: 'row', alignItems:'center', justifyContent:'center'}}>
-            <Typography color={'white'}>
-              DOWNLOAD
-            </Typography>
-            <Icon name='download' size={24} color='white'/>
-          </View>
+          {downloading ? (
+            <View style={{ height:52, flexDirection:'row', alignItems:'center', justifyContent:'center' }}>
+              <ActivityIndicator/>
+            </View>
+          ) : (
+            <View style={{ height:52, flexDirection: 'row', alignItems:'center', justifyContent:'center'}}>
+              <Typography color={'white'}>
+                DOWNLOAD
+              </Typography>
+              <Icon name='download' size={24} color='white'/>
+            </View>
+          )}
         </View>
       </Button>
 
